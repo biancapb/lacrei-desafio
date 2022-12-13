41abb16317e559424142a7a332dd81bcb7c54081
Bianca Barbosa <yume_shiro@hotmail.com>
2022-12-13 20:50:07 -0300

    feat: creating react app and footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { Text } from "./FooterStyle";
+const Footer = (): JSX.Element => {
+  return (
+    <>
+      <Text>Desafio Front-end Lacrei</Text>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/FooterStyle.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Text = styled.span`
+  color: #515151;
+  font-family: "Nunito", sans-serif;
+  font-size: 14px;
+`;
